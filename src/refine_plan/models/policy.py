dd1fbb7903307c38106511d64900a8ac30fa2643
Charlie Street <me@charliestreet.net>
2024-03-18 10:05:32 +0000

    Added value function to policy
    Signed-off-by: Charlie Street <me@charliestreet.net>

--- src/refine_plan/models/policy.py
+++ src/refine_plan/models/policy.py
@@ -11,15 +11,18 @@ class Policy(object):
 
     Attributes:
         _state_action_dict: A dictionary from states to actions
+        _value_dict: A dictionary from states to values under that policy
     """
 
-    def __init__(self, state_action_dict):
+    def __init__(self, state_action_dict, value_dict=None):
         """Initialise attributes.
 
         Args:
             state_action_dict: The state action mapping
+            value_dict: Optional. A state value mapping
         """
         self._state_action_dict = state_action_dict
+        self._value_dict = value_dict
 
     def get_action(self, state):
         """Return the policy action for a given state.
@@ -33,6 +36,26 @@ class Policy(object):
         if state not in self._state_action_dict:
             return None
         return self._state_action_dict[state]
+
+    def get_value(self, state):
+        """Return the value at a given state.
+
+        Args:
+            state: The state we want to retrieve the value for
+
+        Returns:
+            value: The value at state
+
+        Raises:
+            no_value_dict_exception: Raised if there is no value dictionary
+        """
+        if self._value_dict is None:
+            raise Exception("No value dictionary provided to policy")
+
+        if state not in self._value_dict:
+            return None
+
+        return self._value_dict[state]
 
     def __getitem__(self, state):
         """Syntactic sugar for get_action.
--- tests/models/policy_tests.py
+++ tests/models/policy_tests.py
@@ -24,21 +24,35 @@ class PolicyTest(unittest.TestCase):
         }
 
         policy = Policy(state_action_dict)
+        with self.assertRaises(Exception):
+            policy.get_value(State({loc: "v1", battery: "high"}))
+
+        value_dict = {
+            State({loc: "v1", battery: "high"}): 3,
+            State({loc: "v2", battery: "med"}): 1,
+            State({loc: "v3", battery: "low"}): 2,
+        }
+
+        policy = Policy(state_action_dict, value_dict=value_dict)
         for l in loc.get_valid_values():
             for b in battery.get_valid_values():
                 state = State({loc: l, battery: b})
                 if l == "v1" and b == "high":
                     self.assertEqual(policy.get_action(state), "move")
                     self.assertEqual(policy[state], "move")
+                    self.assertEqual(policy.get_value(state), 3)
                 elif l == "v2" and b == "med":
                     self.assertEqual(policy.get_action(state), "charge")
                     self.assertEqual(policy[state], "charge")
+                    self.assertEqual(policy.get_value(state), 1)
                 elif l == "v3" and b == "low":
                     self.assertEqual(policy.get_action(state), "stop")
                     self.assertEqual(policy[state], "stop")
+                    self.assertEqual(policy.get_value(state), 2)
                 else:
                     self.assertEqual(policy.get_action(state), None)
                     self.assertEqual(policy[state], None)
+                    self.assertEqual(policy.get_value(state), None)
 
 
 if __name__ == "__main__":
