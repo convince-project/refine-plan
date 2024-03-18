6d06236ebdab3f24693822c2da510bf487dbfe22
Charlie Street <me@charliestreet.net>
2024-03-18 14:31:06 +0000

    Majority of tests for semi_mdp_solver
    Signed-off-by: Charlie Street <me@charliestreet.net>

--- tests/solver/semi_mdp_solver_tests.py
+++ tests/solver/semi_mdp_solver_tests.py
@@ -11,14 +11,29 @@ from refine_plan.solver.semi_mdp_solver import (
     _build_prism_program,
     _build_storm_model,
     _check_result,
-    _extract_policy,
     synthesise_policy,
 )
 from refine_plan.models.semi_mdp import SemiMDP
 from refine_plan.models.option import Option
 from refine_plan.models.state import State
-from datetime import datetime
 import unittest
+import stormpy
+
+
+# Dummy classes for _check_result test
+class DummyScheduler(object):
+
+    def __init__(self, memoryless, deterministic):
+        self.memoryless = memoryless
+        self.deterministic = deterministic
+
+
+class DummyResult(object):
+
+    def __init__(self, all_states, has_scheduler, memoryless, deterministic):
+        self.result_for_all_states = all_states
+        self.has_scheduler = has_scheduler
+        self.scheduler = DummyScheduler(memoryless, deterministic)
 
 
 def build_test_semi_mdp():
@@ -101,6 +116,56 @@ class BuildPrismProgramTest(unittest.TestCase):
         self.assertEqual(prog.labels[1].name, "label_2")
         self.assertEqual(prog.nr_modules, 1)
         self.assertEqual(prog.model_type.name, "MDP")
+
+
+class BuildStormModelTest(unittest.TestCase):
+
+    def test_function(self):
+
+        semi_mdp = build_test_semi_mdp()
+        prog = _build_prism_program(semi_mdp)
+        formula = stormpy.parse_properties('Rmax=?[F "label_1"]', prog)[0]
+
+        model = _build_storm_model(prog, formula)
+        self.assertEqual(model.nr_states, 6)
+        self.assertEqual(model.nr_transitions, 12)  # This includes added self loops
+        self.assertEqual(len(model.labeling.get_labels()), 4)
+        self.assertTrue("label_1" in model.labeling.get_labels())
+        self.assertTrue("label_2" in model.labeling.get_labels())
+        self.assertTrue("deadlock" in model.labeling.get_labels())
+        self.assertTrue("init" in model.labeling.get_labels())
+        self.assertEqual(model.model_type.name, "MDP")
+
+
+class CheckResultTest(unittest.TestCase):
+
+    def test_function(self):
+
+        for all_states in [False, True]:
+            for has_scheduler in [False, True]:
+                for memoryless in [False, True]:
+                    for deterministic in [False, True]:
+                        result = DummyResult(
+                            all_states, has_scheduler, memoryless, deterministic
+                        )
+
+                        if (
+                            not all_states
+                            or not has_scheduler
+                            or not memoryless
+                            or not deterministic
+                        ):
+                            with self.assertRaises(Exception):
+                                _check_result(result)
+                        else:
+                            _check_result(result)
+
+
+class SynthesisePolicyTest(unittest.TestCase):
+
+    def test_function(self):
+        # Also tests _extract_policy
+        pass
 
 
 if __name__ == "__main__":
