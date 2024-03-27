38d7dc4c20411046212802e34cffcaee418568a9
Charlie Street <me@charliestreet.net>
2024-03-27 15:29:16 +0000

    Build internal mappings test
    Signed-off-by: Charlie Street <me@charliestreet.net>

--- tests/algorithms/policy_to_bt_tests.py
+++ tests/algorithms/policy_to_bt_tests.py
@@ -5,12 +5,13 @@ Author: Charlie Street
 Owner: Charlie Street
 """
 
-from pyeda.boolalg.expr import expr, And, Or
 from refine_plan.algorithms.policy_to_bt import PolicyBTConverter
 from refine_plan.models.state_factor import StateFactor
 from refine_plan.models.condition import EqCondition
 from refine_plan.models.policy import Policy
+from pyeda.boolalg.expr import expr, And, Or
 from refine_plan.models.state import State
+from sympy import Symbol
 import unittest
 
 
@@ -118,6 +119,51 @@ class ExtractRulesFromPolicyTest(unittest.TestCase):
                 )
             )
         )
+
+
+class BuildInternalMappingsTest(unittest.TestCase):
+
+    def test_function(self):
+        converter = PolicyBTConverter()
+
+        sf1 = StateFactor("sf1", ["a", "b", "c"])
+        sf2 = StateFactor("sf2", ["d", "e", "f"])
+
+        state_action_map = {}
+        state_action_map[State({sf1: "a", sf2: "d"})] = "a1"
+        state_action_map[State({sf1: "a", sf2: "e"})] = "a2"
+        state_action_map[State({sf1: "a", sf2: "f"})] = "a3"
+        state_action_map[State({sf1: "b", sf2: "d"})] = "a2"
+        state_action_map[State({sf1: "b", sf2: "e"})] = "a3"
+        state_action_map[State({sf1: "b", sf2: "f"})] = "a1"
+        state_action_map[State({sf1: "c", sf2: "d"})] = "a3"
+        state_action_map[State({sf1: "c", sf2: "e"})] = "a2"
+        state_action_map[State({sf1: "c", sf2: "f"})] = "a1"
+
+        policy = Policy(state_action_map)
+        _, act_to_var_map = converter._extract_rules_from_policy(policy)
+
+        self.assertEqual(converter._vars_to_conds, None)
+        self.assertEqual(converter._vars_to_symbols, None)
+
+        converter._build_internal_mappings(act_to_var_map)
+
+        self.assertEqual(len(converter._vars_to_conds), 6)
+        self.assertEqual(len(converter._vars_to_symbols), 6)
+
+        self.assertEqual(converter._vars_to_conds["sf1EQa"], EqCondition(sf1, "a"))
+        self.assertEqual(converter._vars_to_conds["sf1EQb"], EqCondition(sf1, "b"))
+        self.assertEqual(converter._vars_to_conds["sf1EQc"], EqCondition(sf1, "c"))
+        self.assertEqual(converter._vars_to_conds["sf2EQd"], EqCondition(sf2, "d"))
+        self.assertEqual(converter._vars_to_conds["sf2EQe"], EqCondition(sf2, "e"))
+        self.assertEqual(converter._vars_to_conds["sf2EQf"], EqCondition(sf2, "f"))
+
+        self.assertEqual(converter._vars_to_symbols["sf1EQa"], Symbol("sf1EQa"))
+        self.assertEqual(converter._vars_to_symbols["sf1EQb"], Symbol("sf1EQb"))
+        self.assertEqual(converter._vars_to_symbols["sf1EQc"], Symbol("sf1EQc"))
+        self.assertEqual(converter._vars_to_symbols["sf2EQd"], Symbol("sf2EQd"))
+        self.assertEqual(converter._vars_to_symbols["sf2EQe"], Symbol("sf2EQe"))
+        self.assertEqual(converter._vars_to_symbols["sf2EQf"], Symbol("sf2EQf"))
 
 
 if __name__ == "__main__":
