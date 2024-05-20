a4521dd91f1132efccd40083eb53be0853555b75
Charlie Street <me@charliestreet.net>
2024-05-20 14:16:30 +0100

    setup.py now works
    Signed-off-by: Charlie Street <me@charliestreet.net>

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open("README.md", 'r') as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-   name='refine_plan',
-   version='0.0.1',
-   description='Automatic Behaviour Tree Refinement',
-   long_description=long_description,
-   author='Charlie Street',
-   author_email='c.l.street@bham.ac.uk',
-   packages=['refine_plan'],
-   package_dir = {'': 'src'},
-   install_requires=['pyeda', 'stormpy']
+    name="refine_plan",
+    version="0.0.1",
+    description="Automatic Behaviour Tree Refinement",
+    long_description=long_description,
+    author="Charlie Street",
+    author_email="c.l.street@bham.ac.uk",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=["pyeda", "stormpy", "numpy", "sympy"],
 )
